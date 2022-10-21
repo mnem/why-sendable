91fe9f7072c3534360c4f555ef0079fbcc922fc1
David Wagner <mnem@noiseandheat.com>
2022-10-21 21:44:28 +0100

    Ugly manual fix

--- why-sendable/Votes.swift
+++ why-sendable/Votes.swift
@@ -7,14 +7,19 @@
 
 import Foundation
 
-final class Votes {
+final class Votes: @unchecked Sendable {
+    private var lock = NSLock()
     private(set) var total = 0
     
     func upVote() {
+        lock.lock()
+        defer { lock.unlock() }
         total += 1
     }
     
     func downVote() {
+        lock.lock()
+        defer { lock.unlock() }
         total -= 1
     }
 }
